4eabe2709acffd3d6a40eb3bf15746cff8e2f29f
Michael Burgstaller <git@michaelburgstaller.com>
2021-12-03 21:25:53 +0100

    DAY-03: Calculate collisions for all given configurations

--- day-03-toboggan-trajectory/src/main/java/com/michaelburgstaller/adventofcode/toboggantrajectory/TobogganTrajectory.java
+++ day-03-toboggan-trajectory/src/main/java/com/michaelburgstaller/adventofcode/toboggantrajectory/TobogganTrajectory.java
@@ -33,16 +33,32 @@ public class TobogganTrajectory extends Exercise {
         return coordinates;
     }
 
+    private static Long calculateTreeCollisions(List<String[]> forest, Integer horizontal, Integer vertical) {
+        var coordinates = calculatePath(forest.size(), forest.get(0).length, horizontal, vertical);
+        return coordinates.stream().map(coordinate -> forest.get(coordinate.y)[coordinate.x]).filter(cell -> cell.equalsIgnoreCase("#")).count();
+    }
+
     private static void driveThreeRightAndOneDown(List<String[]> forest) {
-        var coordinates = calculatePath(forest.size(), forest.get(0).length, 3, 1);
-        var trees = coordinates.stream().map(coordinate -> forest.get(coordinate.y)[coordinate.x]).filter(cell -> cell.equalsIgnoreCase("#")).count();
+        var trees = calculateTreeCollisions(forest, 3, 1);
         System.out.println("On your way down, you encountered a total of '" + trees + "' trees.");
+    }
+
+    private static void driveAllGivenConfigurations(List<String[]> forest) {
+        var r1d1 = calculateTreeCollisions(forest, 1, 1);
+        var r3d1 = calculateTreeCollisions(forest, 3, 1);
+        var r5d1 = calculateTreeCollisions(forest, 5, 1);
+        var r7d1 = calculateTreeCollisions(forest, 7, 1);
+        var r1d2 = calculateTreeCollisions(forest, 1, 2);
+        var collisions = r1d1 * r3d1 * r5d1 * r7d1 * r1d2;
+
+        System.out.println("WIth all configurations, you would have had '" + collisions + "' collisions with a tree");
     }
 
     public static void main(String[] args) {
         var forest = getLineStream().map(line -> line.split("")).toList();
 
         driveThreeRightAndOneDown(forest);
+        driveAllGivenConfigurations(forest);
     }
 
 }
