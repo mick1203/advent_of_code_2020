8ef2b1a2d41f35331ad29cab666c54f3305c8d76
Michael Burgstaller <git@michaelburgstaller.com>
2021-12-03 21:32:08 +0100

    DAY-04: Add basic structure

---
+++ day-04-passport-processing/src/main/java/com/michaelburgstaller/adventofcode/passportprocessing/PassportProcessing.java
@@ -0,0 +1,11 @@
+package com.michaelburgstaller.adventofcode.passportprocessing;
+
+import com.michaelburgstaller.adventofcode.Exercise;
+
+public class PassportProcessing extends Exercise {
+
+    public static void main(String[] args) {
+
+    }
+
+}
