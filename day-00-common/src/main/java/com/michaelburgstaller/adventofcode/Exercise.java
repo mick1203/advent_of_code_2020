2c62d4abe9bb74b884191ff2a57e8d35a0f60707
Michael Burgstaller <git@michaelburgstaller.com>
2021-12-04 22:03:31 +0100

    AOC: Extend common base class

--- day-00-common/src/main/java/com/michaelburgstaller/adventofcode/Exercise.java
+++ day-00-common/src/main/java/com/michaelburgstaller/adventofcode/Exercise.java
@@ -1,7 +1,9 @@
-package com.michaelburgstaller.adventofcode;
+package com.michaelburgstaller.adventofcode.common;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Exercise {
@@ -13,7 +15,33 @@ public class Exercise {
     }
 
     protected static Stream<String> getLineStream() {
-        return getFileReader("input.txt").lines();
+        return getLineStream("input.txt");
+    }
+
+    protected static Stream<String> getLineStream(String path) {
+        return getFileReader(path).lines();
+    }
+
+    protected static Stream<List<String>> getBufferedLineStream() {
+        return getBufferedLineStream(getLineStream(), "");
+    }
+
+    protected static Stream<List<String>> getBufferedLineStream(Stream<String> lineStream, String separator) {
+        var data = lineStream.toList();
+        var batches = new ArrayList<List<String>>();
+        var batch = new ArrayList<String>();
+
+        for (var line : data) {
+            if (line.contentEquals(separator)) {
+                batches.add(List.copyOf(batch));
+                batch.clear();
+                continue;
+            }
+            batch.add(line.strip());
+        }
+        batches.add(batch);
+
+        return batches.stream();
     }
 
 }
